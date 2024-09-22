98a21fe9b3a52b3f0ad6ed89e2f17ca9b8d59063
hamidusman <abdulhamidusman218@gmail.com>
2024-09-22 13:42:36 +0100

    rooms design

--- SovnGaard/src/App.jsx
+++ SovnGaard/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './header'
-import Hero from './hero'
-import About from './about'
-import Review from './review'
-import Contact from './contact'
+import Header from './base-components/header'
+import Rooms from './rooms'
+/*
 
+import Hero from './home/hero'
+import About from './home/about'
+import Review from './home/review'
+import Contact from './home/contact'
+*/
 function App(){
   
   return (
     <>
     <Header />
-    <Hero />
-    <About />
-    <Review />
-    <Contact />
+    <Rooms />
     
     </>
   )
---
+++ SovnGaard/src/rooms.jsx
@@ -0,0 +1,44 @@
+
+
+function Rooms(){
+    return(
+        <section className="text-smoke px-20">
+            <h1 className="text-[35px] text-center mt-10">Take A Look At Our Available Rooms</h1>
+
+            <div className="my-10 flex gap-10 justify-center">
+                <article>
+                    <div className="img w-[340px] h-[280px] bg-primary my-1"></div>
+
+                    <div className="texts">
+                        <p className="mb-1">Title: Da Classica</p>
+                        <p className="mb-1">Price: <span className="font-bold">$5/Night</span></p>
+                        <p className="mb-1">Available Rooms: <span className="font-bold">2/10</span></p>
+                    </div>
+
+                </article>
+                <article>
+                    <div className="img w-[340px] h-[280px] bg-primary my-1"></div>
+
+                    <div className="texts">
+                        <p className="mb-1">Title: Da Classica</p>
+                        <p className="mb-1">Price: <span className="font-bold">$5/Night</span></p>
+                        <p className="mb-1">Available Rooms: <span className="font-bold">2/10</span></p>
+                    </div>
+
+                </article>
+                <article>
+                    <div className="img w-[340px] h-[280px] bg-primary my-1"></div>
+
+                    <div className="texts">
+                        <p className="mb-1">Title: Da Classica</p>
+                        <p className="mb-1">Price: <span className="font-bold">$5/Night</span></p>
+                        <p className="mb-1">Available Rooms: <span className="font-bold">2/10</span></p>
+                    </div>
+
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Rooms
